04af7e894e5a78f6d6f86de9feccb320bf3b2db0
Daan Siepelinga <daan-siepelinga@hotmail.com>
2016-05-27 15:40:16 +0200

    Log view now contains non interactive commit boxes filled with the past commit ids

--- gui/widgets/commitboxview.py
+++ gui/widgets/commitboxview.py
@@ -1,6 +1,11 @@
 from kivy.uix.boxlayout import BoxLayout
 
 from gui.eventwidget import EventWidget
+from gui.widgets.labelrecolorable import LabelRecolorable
+
+
+class CommitBox(LabelRecolorable):
+  pass
 
 
 class CommitBoxView(BoxLayout, EventWidget):
@@ -10,3 +15,5 @@ class CommitBoxView(BoxLayout, EventWidget):
 
   def receive_event_result(self, **kwargs):
     print kwargs
+    for commit_hex in kwargs["data"]:
+      self.add_widget(CommitBox(text=commit_hex[:5]))
