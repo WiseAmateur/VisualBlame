b42e2cf944823038a1ddac7e14922df7247d5d04
Daan Siepelinga <daan-siepelinga@hotmail.com>
2016-06-03 12:22:50 +0200

    Fixed a bug where the log module initially goes through all logs, improved log boundaries to always have the same amount of logs on the screen if possible

--- modules/log.py
+++ modules/log.py
@@ -16,17 +16,18 @@ class Log(GitModuleBase):
 
     if self.commit_id == "HEAD":
       self.commit_id = log_data[0]
+    else:
+      while True:
+        try:
+          commit_id = walker.next().hex
+        except StopIteration:
+          walker = None
+          break
 
-    while True:
-      try:
-        commit_id = walker.next().hex
-      except StopIteration:
-        walker = None
-        break
-      log_data.append(commit_id)
+        log_data.append(commit_id)
 
-      if commit_id == self.commit_id:
-        break
+        if commit_id == self.commit_id:
+          break
 
     if walker:
       for i in range(0, self.amount):
@@ -35,10 +36,29 @@ class Log(GitModuleBase):
         except StopIteration:
           break
 
+
     commit_index = log_data.index(self.commit_id)
-    if commit_index <= self.amount / 2:
-      result_data = log_data[0:self.amount]
-    else:
-      result_data = log_data[commit_index - self.amount / 2:commit_index + self.amount / 2]
+    result_data = self._reduce_log_result_to_amount(commit_index, log_data, self.amount)
 
     super(Log, self).return_final_result({"commit_id": result_data})
+
+  def _reduce_log_result_to_amount(self, active_index, log_data, amount):
+    reduced_data = [log_data[active_index]]
+    half_amount = amount / 2
+
+    # Try to fill the amount of entries with half the amount on both boundaries
+    min_bound = max(0, active_index - half_amount)
+    max_bound = min(len(log_data), active_index + half_amount + 1)
+
+    # If one of the boundaries causes less than the half amount of entries
+    # next to the active commit, fill the total amoun with more entries
+    # from the other boundary if possible
+    min_bound_diff = active_index - min_bound
+    if min_bound_diff < half_amount:
+      max_bound = min(len(log_data), max_bound + half_amount - min_bound_diff)
+
+    max_bound_diff = max_bound - active_index
+    if max_bound_diff < half_amount:
+      min_bound = max(0, min_bound - (half_amount - max_bound_diff) - 1)
+
+    return log_data[min_bound:max_bound]
