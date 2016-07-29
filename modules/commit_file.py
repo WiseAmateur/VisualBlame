895f3ea3784edb9be6f207e830767a70ddf6eea8
Daan Siepelinga <daan-siepelinga@hotmail.com>
2016-07-29 09:34:39 +0200

    Added new module to get the file contents in a specific commit. Should result in cleaner code and should avoid problems with untracked lines at the start.

---
+++ modules/commit_file.py
@@ -0,0 +1,40 @@
+import pygit2
+from collections import namedtuple
+
+from modules.modulebase import GitModuleBase
+
+
+class CommitFile(GitModuleBase):
+    def __init__(self, commit_id="", file_path="", **kwargs):
+        self.commit_id = commit_id
+        self.file_path = file_path
+        self.key = self.commit_id + self.file_path
+        super(CommitFile, self).__init__(**kwargs)
+
+    def get_result_from_cache(self, data):
+        try:
+            return data[self.key]
+        # TypeError if the data is None, KeyError if the data doesn't
+        # contain the key of this module
+        except (TypeError, KeyError):
+            return None
+
+    def execute(self):
+        CommitFileData = namedtuple("CommitFileData",
+                                    ["file_path", "commit_id", "lines"])
+
+        try:
+            commit_tree = self._repo.get(str(self.commit_id)).tree
+        except ValueError:
+            # When the input commit id is not an id, but something like HEAD
+            commit_tree = self._repo.revparse_single(self.commit_id).tree
+
+        for path_name in self.file_path.split("/"):
+            commit_tree = commit_tree[path_name]
+
+        lines = self._repo.get(str(commit_tree.id)).data.splitlines()
+
+        result = CommitFileData(self.file_path, self.commit_id, lines)
+
+        super(CommitFile, self).return_cache_result(self.key, result)
+        super(CommitFile, self).return_final_result(result)
--- modules/module_event_config.py
+++ modules/module_event_config.py
@@ -1,4 +1,5 @@
 from modules.blame import Blame
+from modules.commit_file import CommitFile
 from modules.commit_context import CommitContext
 from modules.diff import Diff
 from modules.log import Log
@@ -6,6 +7,7 @@ from modules.log import Log
 
 module_events = {
     "blame": Blame,
+    "commit_file": CommitFile,
     "commit_context": CommitContext,
     "diff": Diff,
     "log": Log
